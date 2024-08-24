3c071d9229cbe70357d610d909eb4b8493e97a52
rollback3 <37938186+rollback3@users.noreply.github.com>
2024-08-24 15:15:01 +0200

    Add project files.

---
+++ vsDbProject.Database/Scripts/Insert_POST_TYPES.sql
@@ -0,0 +1,18 @@
+﻿
+MERGE INTO [Lookup].[POST_TYPES] AS TARGET 
+USING (VALUES 	
+	(1, N'Music'),
+	(2, N'Art'),
+	(3, N'History'),
+	(4, N'Computer Science')
+) 
+AS SOURCE (Id, [Description]) 
+ON TARGET.Id = SOURCE.Id
+-- update matched rows 
+WHEN MATCHED THEN 
+UPDATE SET [Description] = SOURCE.[Description]
+-- insert new rows 
+WHEN NOT MATCHED BY TARGET THEN 
+INSERT (Id, [Description]) 
+VALUES (Id, [Description]);
+
---
+++ vsDbProject.Database/dbo/Tables/POSTS.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[POSTS]
+(
+	[Id]                INT IDENTITY(1,1)           NOT NULL,
+	[IdType]            INT                         NOT NULL,
+    [Title]             VARCHAR(1000)               NOT NULL,
+    [Published]         BIT                         NOT NULL,
+    [ImageURL]          VARCHAR(1000)               NULL,
+    [Content]           VARCHAR(5000)               NOT NULL,
+    CONSTRAINT [PK_POSTS] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_POSTS_TYPE] FOREIGN KEY ([IdType]) REFERENCES [Lookup].[POST_TYPES] ([Id])
+)
